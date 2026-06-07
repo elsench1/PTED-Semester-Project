f46b1e27c2be2d20d3c614e4365f46213a03b007
Christopher <162795219+elsench1@users.noreply.github.com>
2026-06-07 11:55:49 +0200

    safe draft file before lunch

--- draft.R
+++ draft.R
@@ -1,28 +1,81 @@
 # Draft
 
-
-
-
-
-
-source("src/R/move_points_to_circle_exit.R")
+# source("src/R/move_points_to_circle_exit.R")
 source("src/R/dataPreparation.R")
-
-
-
-
-
-# library(tmap)
-
-
 
 GPXFileLink <- "data/rawData/Trackingdata_Christopher/20260312-172538.gpx"
 
-# GPS_Track <- load_GPX_File(GPXFile = GPXFileLink)
 
-hide_points <- get_df_of_hide_points(
-  hide_point_csv = "data/metaData/hidePoint.csv",
-  target_crs = 2056)
+
+GPS_Track <- load_GPX_File(GPXFile = GPXFileLink) |> 
+  remove_all_na_columns() |> 
+  add_speed_and_accel_to_GSP_df() |> 
+  mark_suspicious_points()
+
+library(dplyr)
+GPS_Track <- GPS_Track |> 
+  filter(bad_point != TRUE | is.na(bad_point))
+# 
+# 
+# library(ggplot2)
+# library(dplyr)
+# library(sf)
+# 
+# GPS_Track_plot <- GPS_Track |>
+#   mutate(
+#     point_type = case_when(
+#       is_suspicious_spike == TRUE & suspicious_accel == TRUE ~ "both",
+#       is_suspicious_spike == TRUE ~ "spike",
+#       suspicious_accel == TRUE ~ "accel",
+#       TRUE ~ "normal"
+#     ),
+#     point_type = factor(
+#       point_type,
+#       levels = c("normal", "spike", "accel", "both")
+#     )
+#   )
+# 
+# ggplot(GPS_Track_plot) +
+#   geom_sf(aes(color = point_type), size = 1.8, alpha = 0.8) +
+#   scale_color_manual(
+#     values = c(
+#       "normal" = "black",
+#       "spike"  = "red",
+#       "accel"  = "green",
+#       "both"   = "blue"
+#     ),
+#     labels = c(
+#       "normal" = "normal",
+#       "spike"  = "is_suspicious_spike",
+#       "accel"  = "suspicious_accel",
+#       "both"   = "beides"
+#     ),
+#     name = "Punkttyp"
+#   ) +
+#   theme_minimal() +
+#   labs(
+#     title = "GPS Track mit markierten verdächtigen Punkten",
+#     x = "Koordinate X",
+#     y = "Koordinate Y"
+#   )
+
+
+
+sum(GPS_Track$is_suspicious_spike, na.rm = TRUE)
+sum(GPS_Track$suspicious_accel, na.rm = TRUE)
+sum(GPS_Track$bad_point, na.rm = TRUE)
+
+library(tmap)
+
+
+
+
+
+
+
+# hide_points <- get_df_of_hide_points(
+#   hide_point_csv = "data/metaData/hidePoint.csv",
+#   target_crs = 2056)
 
 # if (is.null(GPS_Track)) {
 #   stop("GPX-Datei konnte nicht geladen werden.")
@@ -34,16 +87,16 @@ hide_points <- get_df_of_hide_points(
 #   dist = 300
 # )
 # 
-# GPS_Track_line <- GPS_Track_hidden |>
-#   summarise(do_union = FALSE) |>
-#   st_cast("LINESTRING")
-# 
-# tmap_mode("view")
-# 
-# map <- tm_shape(GPS_Track_line) +
-#   tm_lines(col = "green", lwd = 5)
-# 
-# print(map)
+GPS_Track_line <- GPS_Track |>
+  summarise(do_union = FALSE) |>
+  st_cast("LINESTRING")
+
+tmap_mode("view")
+
+map <- tm_shape(GPS_Track_line) +
+  tm_lines(col = "green", lwd = 5)
+
+print(map)
 # 
 
 
