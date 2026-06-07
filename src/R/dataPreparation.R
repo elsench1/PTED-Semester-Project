ec53aa917088497cef7d1e78f1817ce9a71670f2
Christopher <162795219+elsench1@users.noreply.github.com>
2026-06-07 10:19:46 +0200

    add mark_suspicious_spikes to dataPreparation.R und librarys (also necessary for other functions)

--- src/R/dataPreparation.R
+++ src/R/dataPreparation.R
@@ -1,6 +1,7 @@
 # This collection of functions helps with data preparation
 library(sf)
-
+library(dplyr)
+library(lubridate)
 
 load_GPX_File <- function(GPXFile){
   data <- tryCatch(
@@ -40,5 +41,32 @@ add_speed_and_accel_to_GSP_df <- function(gpx) {
       speed_calc = dist_m / dt,
       speed_kmh = speed_calc * 3.6,
       accel = (speed_calc - lag(speed_calc)) / dt
+    )
+}
+
+mark_suspicious_spikes <- function(df,
+                                   jump_m = 80,
+                                   return_m = 30,
+                                   max_dt = 20) {
+  df |>
+    mutate(
+      x_prev = lag(x),
+      y_prev = lag(y),
+      x_next = lead(x),
+      y_next = lead(y),
+      
+      # Pythagoras' theorem
+      dist_prev = sqrt((x - x_prev)^2 + (y - y_prev)^2),
+      dist_next = sqrt((x_next - x)^2 + (y_next - y)^2),
+      dist_skip = sqrt((x_next - x_prev)^2 + (y_next - y_prev)^2),
+      
+      dt_prev = as.numeric(difftime(time, lag(time), units = "secs")),
+      dt_next = as.numeric(difftime(lead(time), time, units = "secs")),
+      
+      is_suspicious_spike = dist_prev > jump_m &
+        dist_next > jump_m &
+        dist_skip < return_m &
+        dt_prev <= max_dt &
+        dt_next <= max_dt
     )
 }
