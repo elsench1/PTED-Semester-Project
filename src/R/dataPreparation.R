fe206b76e06bdce499d2f1f89bc974f56f7acdbf
Christopher <162795219+elsench1@users.noreply.github.com>
2026-06-07 09:52:29 +0200

    Add add_speed_and_accel_to_GPS_df to dataPreparation.R

--- src/R/dataPreparation.R
+++ src/R/dataPreparation.R
@@ -22,3 +22,23 @@ remove_all_na_columns <- function(data) {
   data[, colSums(!is.na(data)) > 0, drop = FALSE]
 }
 
+add_speed_and_accel_to_GSP_df <- function(gpx) {
+  gpx <- gpx |>
+    arrange(time)
+  
+  coords <- st_coordinates(gpx)
+  
+  gpx |>
+    mutate(
+      x = coords[, 1],
+      y = coords[, 2],
+      time = ymd_hms(time, tz = "UTC"),
+      dt = as.numeric(difftime(time, lag(time), units = "secs")),
+      dx = x - lag(x),
+      dy = y - lag(y),
+      dist_m = sqrt(dx^2 + dy^2),
+      speed_calc = dist_m / dt,
+      speed_kmh = speed_calc * 3.6,
+      accel = (speed_calc - lag(speed_calc)) / dt
+    )
+}
