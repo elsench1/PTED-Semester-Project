aee5de1bd84efdbeda6b97717d8f7fd30b993e6e
Christopher <162795219+elsench1@users.noreply.github.com>
2026-06-08 22:48:16 +0200

    safe draft.R

--- draft.R
+++ draft.R
@@ -2,6 +2,8 @@
 
 source("src/R/move_points_to_circle_exit.R")
 source("src/R/dataPreparation.R")
+source("src/R/gps_track_segmentation.R")
+source("src/R/compress_stop_blocks.R")
 # 
 # GPXFileLink <- "data/rawData/Trackingdata_Christopher/20260312-172538.gpx"
 # 
@@ -67,45 +69,135 @@ source("src/R/dataPreparation.R")
 # 
 # library(tmap)
 
+#######################################################################3
 
-
+# Datei Speichern und laden
 # saveRDS(GPS_Track, "data/processedData/GPS_Track_processed.rds")
 
 GPS_Track <- readRDS("data/processedData/GPS_Track_processed.rds")
 
+############################################################################
+# Eigen Regeln für gewisse Gebiete
 
-home <- st_sfc(
-  st_point(c(8.519625,47.156997)),
-  crs = 4326
-) |> 
-  st_transform(crs =2056)
+# home <- st_sfc(
+#   st_point(c(8.519625,47.156997)),
+#   crs = 4326
+# ) |> 
+#   st_transform(crs =2056)
+# 
+# home_buffer <- st_buffer(home, 300)
+# 
+# GPS_Track <- GPS_Track |> 
+#   mutate(
+#     near_home = as.logical(st_intersects(geometry, home_buffer, sparse = FALSE)[, 1]),
+#     bad_near_home = near_home & speed_kmh > 50
+#   )
+# 
+# sum(GPS_Track$bad_near_home, na.rm = TRUE)
+# 
+# library(dplyr)
+# GPS_Track <- GPS_Track |>
+#   filter(bad_near_home != TRUE | is.na(bad_near_home))
+# 
+# 
+# GPS_Track <- GPS_Track |>
+#   mutate(
+#     low_sat = sat < 6
+#   )
+# 
+# sum(GPS_Track$low_sat, na.rm = TRUE)
+# 
+# GPS_Track <- GPS_Track |> 
+#   filter(low_sat != TRUE | is.na(low_sat))
 
-home_buffer <- st_buffer(home, 300)
+################################################################
+# Segment
 
-GPS_Track <- GPS_Track |> 
+
+
+
+
+
+# GPS_Track <- detect_stops(GPS_Track)
+
+test <- GPS_Track[1:10000, ]
+system.time(
+  result <- detect_stops(test)
+)
+
+
+
+
+df_test <- GPS_Track[1:10000, ]
+
+system.time({
+  result_test <- detect_stops_rcpp(df_test)
+})
+
+
+GPS_Track <- detect_stops_rcpp(GPS_Track)
+GPS_Track <- segment_tracks(GPS_Track)
+
+#####################################################################
+
+# remove stop blocks
+
+GPS_Track <- GPS_Track |>
+  arrange(time) |>
   mutate(
-    near_home = as.logical(st_intersects(geometry, home_buffer, sparse = FALSE)[, 1]),
-    bad_near_home = near_home & speed_kmh > 50
+    bad_point = coalesce(bad_point, FALSE),
+    is_suspicious_spike = coalesce(is_suspicious_spike, FALSE),
+    suspicious_accel = coalesce(suspicious_accel, FALSE),
+    is_stop = coalesce(is_stop, FALSE),
+    moving = coalesce(moving, FALSE),
+    new_segment = if_else(row_number() == 1, TRUE, coalesce(new_segment, FALSE))
   )
 
-sum(GPS_Track$bad_near_home, na.rm = TRUE)
+GPS_Track_compressed <- compress_stop_blocks(GPS_Track)
 
-library(dplyr)
-GPS_Track <- GPS_Track |>
-  filter(bad_near_home != TRUE | is.na(bad_near_home))
+nrow(GPS_Track)
+nrow(GPS_Track_compressed)
 
+GPS_Track_compressed <- GPS_Track_compressed |>
+  select(
+    -any_of(c(
+      "dt", "dx", "dy", "dist_m", "speed_calc", "speed_kmh", "accel",
+      "x_prev", "y_prev", "x_next", "y_next",
+      "dist_prev", "dist_next", "dist_skip",
+      "dt_prev", "dt_next"
+    ))
+  )
+#########################################################
 
-GPS_Track <- GPS_Track |>
-  mutate(
-    low_sat = sat < 6
+library(tmap)
+library(colorspace)
+
+GPS_segments <- GPS_segments |>
+  mutate(segment_id = as.factor(segment_id))
+
+segment_palette <- qualitative_hcl(
+  n = nlevels(GPS_segments$segment_id),
+  palette = "Dark 3"
+)
+
+tmap_mode("view")
+
+tm_basemap("OpenStreetMap") +
+  tm_shape(st_transform(GPS_segments, 4326)) +
+  tm_lines(
+    col = "segment_id",
+    lwd = 3,
+    palette = segment_palette,
+    legend.col.show = FALSE,
+    popup.vars = c(
+      "Segment" = "segment_id",
+      "Start" = "start_time",
+      "Ende" = "end_time",
+      "Punkte" = "n_points"
+    )
   )
 
-sum(GPS_Track$low_sat, na.rm = TRUE)
-
-GPS_Track <- GPS_Track |> 
-  filter(low_sat != TRUE | is.na(low_sat))
-
-
+#############################################################
 # hide_points <- get_df_of_hide_points(
 #   hide_point_csv = "data/metaData/hidePoint.csv",
 #   target_crs = 2056)
@@ -120,19 +212,22 @@ GPS_Track <- GPS_Track |>
 #   dist = 300
 # )
 # 
-library(tmap)
 
-GPS_Track_line <- GPS_Track |>
-  summarise(do_union = FALSE) |>
-  st_cast("LINESTRING")
-
-tmap_mode("view")
-
-map <- tm_shape(GPS_Track_line) +
-  tm_lines(col = "green", lwd = 5)
-
-print(map)
-# # 
+####################################################################
+# tmap Plot
+# library(tmap)
+# 
+# GPS_Track_line <- GPS_Track |>
+#   summarise(do_union = FALSE) |>
+#   st_cast("LINESTRING")
+# 
+# tmap_mode("view")
+# 
+# map <- tm_shape(GPS_Track_line) +
+#   tm_lines(col = "green", lwd = 5)
+# 
+# print(map)
+# # #######################################################################
 
 
 # main <- function() {
