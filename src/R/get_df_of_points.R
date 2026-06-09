e4e512e9ee6a3fb967f78cfb055b60e1850df9a2
Christopher <162795219+elsench1@users.noreply.github.com>
2026-06-09 16:16:39 +0200

    add radius to get df of points

--- src/R/get_df_of_points.R
+++ src/R/get_df_of_points.R
@@ -21,28 +21,35 @@ get_df_of_points <- function(csv_file_with_points, target_crs = 2056) {
     return(NULL)
   }
   
-  required_cols <- c("ID","x", "y", "crs")
+  required_cols <- c("ID", "x", "y", "r", "crs")
   
   if (!all(required_cols %in% names(csv_data))) {
     stop(
-      "CSV muss die Spalten ID, x, y und crs enthalten. ",
-      "Beispiel: ID,x,y,crs"
+      "CSV muss die Spalten ID, x, y, r und crs enthalten. ",
+      "Beispiel: ID,x,y,r,crs"
     )
   }
   
   csv_data$ID <- as.character(csv_data$ID)
   csv_data$x <- as.numeric(csv_data$x)
   csv_data$y <- as.numeric(csv_data$y)
+  csv_data$r <- as.numeric(csv_data$r)
   csv_data$crs <- as.integer(csv_data$crs)
   
-  if (any(is.na(csv_data$x)) || any(is.na(csv_data$y)) || any(is.na(csv_data$crs))) {
-    stop("CSV enthält ungültige Werte in x, y oder crs.")
+  if (
+    any(is.na(csv_data$x)) ||
+    any(is.na(csv_data$y)) ||
+    any(is.na(csv_data$r)) ||
+    any(is.na(csv_data$crs))
+  ) {
+    stop("CSV enthält ungültige Werte in x, y, r oder crs.")
   }
   
-  hide_points_list <- lapply(seq_len(nrow(csv_data)), function(i) {
+  points_list <- lapply(seq_len(nrow(csv_data)), function(i) {
     
     point_df <- data.frame(
       ID = csv_data$ID[i],
+      r = csv_data$r[i],
       x = csv_data$x[i],
       y = csv_data$y[i]
     )
@@ -55,7 +62,7 @@ get_df_of_points <- function(csv_file_with_points, target_crs = 2056) {
       st_transform(crs = target_crs)
   })
   
-  hide_points <- do.call(rbind, hide_points_list)
+  points <- do.call(rbind, points_list)
   
-  return(hide_points)
+  return(points)
 }
